9f540bf9472e534bbdf1dfa91ea7b21594700607
Chen Jincong <38436918+AnonymousC2017@users.noreply.github.com>
2023-10-21 09:04:16 +0800

    1

--- doctorbot/main.sh
+++ doctorbot/main.sh
@@ -5,4 +5,4 @@ DATE="11-01"
 SITE = "http://www.fjsdsrmyy.com/showdoc.aspx?Id=4bbb332d-2c77-4d03-8f94-0a350343"
 
 
-python doctorbot/spider.py "${DATE}" "${SITE}"
+python doctorbot/spider.py ${DATE} ${SITE}
