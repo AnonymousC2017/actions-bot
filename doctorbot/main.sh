d30856fb60b9cf933152e63ad204848a2e2f1c42
Chen Jincong <38436918+AnonymousC2017@users.noreply.github.com>
2023-10-21 10:57:48 +0800

    Update main.sh

--- doctorbot/main.sh
+++ doctorbot/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eux
-DATE="11-04"
-SITE="http://www.fjsdsrmyy.com/showdoc.aspx?Id=4bbb332d-2c77-4d03-8f94-0a350343"
+DATE="10-23"
+SITE="http://www.fjsdsrmyy.com/showdoc.aspx?Id=4bbb332d-2c77-4d03-8f94-0a350346"
 
 python doctorbot/spider.py ${DATE} ${SITE}
