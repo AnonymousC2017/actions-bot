a51dc401efbc663115b6f9c7235ab037969a9711
Chen Jincong <38436918+AnonymousC2017@users.noreply.github.com>
2023-10-26 09:48:39 +0800

    Update spider.py

--- doctorbot/spider.py
+++ doctorbot/spider.py
@@ -1,19 +1,17 @@
 #!/bin/python
-#Coding="utf-8"
-from lxml import etree
-import sys
-import requests
-import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
+# Coding="utf-8"
 import os
-from wechatpy import WeChatClient
-from wechatpy.client.api import WeChatMessage, WeChatTemplate
-from datetime import datetime
-import pytz
 import re
+from datetime import datetime
 
-def getResult(DATE,SITE):
+import pytz
+import requests
+from lxml import etree
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage
+
+
+def getResult(DATE, SITE):
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
     }
@@ -34,16 +32,16 @@ def getResult(DATE,SITE):
                 bookable.append(time)
             else:
                 full.append(time)
-    return flag,bookable,full,doctor_name
+    return flag, bookable, full, doctor_name
 
 
 # 保存email内容
 def saveEmail(email_path, message):
-
     with open(email_path, 'w', encoding="utf-8") as email:
         email.writelines(message)
 
-def sendWx(bookable_list,full_list,BOOK_DATE,doctor_name):
+
+def sendWx(bookable_list, full_list, BOOK_DATE, doctor_name):
     app_id = os.environ["APP_ID"]
     app_secret = os.environ["APP_SECRET"]
     user_ids = os.environ["USER_ID"].split(',')
@@ -54,11 +52,11 @@ def sendWx(bookable_list,full_list,BOOK_DATE,doctor_name):
     now = datetime.now(china_tz)
     now_formatted = now.strftime('%Y-%m-%d %H:%M')
     data = {
-        "now_formatted": {"value":now_formatted},
-        "doctor_name": {"value":doctor_name},
-        "bookable_list":{"value":bookable_list},
-        "full_list":{"value":full_list},
-        "BOOK_DATE": {"value":BOOK_DATE},
+        "now_formatted": {"value": now_formatted},
+        "doctor_name": {"value": doctor_name},
+        "bookable_list": {"value": bookable_list},
+        "full_list": {"value": full_list},
+        "BOOK_DATE": {"value": BOOK_DATE},
     }
 
     for i in range(len(user_ids)):
@@ -73,28 +71,25 @@ if __name__ == "__main__":
     BOOK_DATE = os.environ["BOOK_DATE"]
     SITE = os.environ["SITE"]
 
-
-    flag,bookable,full,doctor_name = getResult(BOOK_DATE,SITE)
+    flag, bookable, full, doctor_name = getResult(BOOK_DATE, SITE)
     bookable_list = []
     full_list = []
-    if flag: # 出号了
+    if flag:  # 出号了
         if len(bookable) > 0:
             for b in bookable:
-                bookable_list += b
+                bookable_list.append(b)
         if len(full) > 0:
             for f in full:
-                full_list += f
+                full_list.append(f)
 
         if len(bookable) == 0 and len(full) > 0:
 
-            match = re.findall(r'(?<=-)\d{2}-\d{2}(?=[^\d])',full[-1])[0]
+            match = re.findall(r'(?<=-)\d{2}-\d{2}(?=[^\d])', full[-1])[0]
             if match == BOOK_DATE[-1]:
                 full_list = "全部约满了 请重新定一个日期"
-
-        sendWx(bookable_list,full_list, BOOK_DATE, doctor_name) # 出号了才向微信推消息。
-        email_message = FORMATED_MESSAGE.format(bookable_list,full_list)
+        sendWx(bookable_list, full_list, BOOK_DATE, doctor_name)  # 向微信推消息。
+        email_message = FORMATED_MESSAGE.format(bookable_list, full_list)
     else:
         email_message = "全都未出号"
     email_path = "email.txt"
-    saveEmail(email_path,email_message)
-    
+    saveEmail(email_path, email_message)
